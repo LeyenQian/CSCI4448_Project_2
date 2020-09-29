597b9bbb2db7e3ce3ec08aa74be972c3552feff7
Leyen Qian <leyenqian@gmail.com>
2020-09-29 14:41:10 -0600

    delegation, panda's eat method

--- Animals/Ursidae/Breed/Panda.java
+++ Animals/Ursidae/Breed/Panda.java
@@ -1,11 +1,21 @@
 package Animals.Ursidae.Breed;
 import Animals.Ursidae.Ursidae;
+import Animals.Ursidae.UrsidaeDelegation;
 
 
 public class Panda extends Ursidae
 {
+    // delegation
+    private UrsidaeDelegation ursidae_delegation = new UrsidaeDelegation();
+
     public Panda(String name)
     {
         super(name);
+    }
+
+    // call the eat method from <ursidae_delegation> instead of the original one that inherited from <animal>
+    public void eat()
+    {
+        ursidae_delegation.eat_for_panda(this.type, this.name);
     }
 }
---
+++ Animals/Ursidae/UrsidaeDelegation.java
@@ -0,0 +1,9 @@
+package Animals.Ursidae;
+
+public class UrsidaeDelegation
+{
+    public void eat_for_panda(String type, String name)
+    {
+        System.out.println(String.format("#[%s] %s eats lots of bamboos.", type, name));
+    }    
+}
